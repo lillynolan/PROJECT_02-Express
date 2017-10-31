5d6de5686aedc44054808128736e6efad61954d2
Lilly Nolan <lillynolan@gmail.com>
2017-10-30 23:49:06 -0400

    updated server.js w new auth user routes

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
-app.use(logger('dev'));
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 const app = express();
 require('dotenv').config();
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -32,8 +33,19 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/', (req, res) => {
+  res.render('index', {
+    auth: (req.user) ? true : false,
+  });
+});
+
 const workoutRoutes = require('./routes/workout-routes');
 app.use('/workout', workoutRoutes);
+
+const authRoutes = require('./routes/auth-routes');
+app.use('/auth', authRoutes);
+const userRoutes = require('./routes/user-routes');
+app.use('/user', userRoutes);
 
 app.use('*', (req, res) => {
   res.status(404).send({
