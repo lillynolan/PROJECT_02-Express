860791af7ae4f3cecbbb5f9b4a40b4d285585405
Lilly Nolan <lillynolan@gmail.com>
2017-10-31 12:40:06 -0400

    Added controller for goals

---
+++ controllers/goal-controller.js
@@ -0,0 +1,80 @@
+const Goal = require('../models/Goal');
+
+const goalController = {};
+
+goalController.index = (req, res) => {
+  Goal.findAll().then(goals => {
+    res.status(200).render('goals/goals-index', {
+      goals: goals,
+      auth: (req.user) ? true : false,
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+};
+
+goalController.show = (req, res) => {
+  Goal.findById(req.params.id)
+  .then(goal => {
+    res.status(200).render('goals/goals-show', {
+      goal: goal,
+      auth: (req.user) ? true : false,
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+};
+
+goalController.create = (req, res) => {
+  Goal.create({
+    name: req.body.name,
+    description: req.body.description,
+  }, req.user.id).then(goal => {
+    res.redirect(`/goals/${goal.id}`)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+};
+
+goalController.edit = (req, res) => {
+  Goal.findById(req.params.id)
+  .then(goal => {
+    res.status(200).render('goals/goals-edit', {
+      goal: goal,
+      auth: (req.user) ? true : false,
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+};
+
+goalController.update = (req, res) => {
+  Goal.update({
+    name: req.body.name,
+    description: req.body.description,
+  }, req.params.id)
+  .then(goal => {
+    res.redirect(`/goals/${goal.id}`)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+};
+
+goalController.delete = (req, res) => {
+  Goal.destroy(req.params.id).then(() => {
+    res.redirect('/goals');
+  }).catch(err => {
+    res.status(500).json({error: err});
+  });
+};
+
+module.exports = goalController
+
+
+
+
