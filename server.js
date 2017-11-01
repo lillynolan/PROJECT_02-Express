6121d24c4205a63c1d5844183ae57be84c33ce81
Lilly Nolan <lillynolan@gmail.com>
2017-11-01 13:47:24 -0400

    server.js update

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
